0bae3bcc856b146f470711521c35db8e99118b62
Administrator <admin@example.com>
2019-10-10 19:07:18 -0700

    Add helper scripts to manual test controllers

---
+++ helper/manual_test_01.sh
@@ -0,0 +1,76 @@
+
+# Create a new user
+echo "Creating user"
+curl -H "Accept: application/json" -H "Content-Type: application/json" -d @helper/user_create_01.json http://localhost:8080/api/user/create
+echo
+echo
+sleep 1
+
+# Get user by id
+echo "Get user by id"
+curl -H "Content-Type: application/json"  http://localhost:8080/api/user/id/1
+echo
+echo
+sleep 1
+
+# Get user by username
+echo "Get user by username"
+curl -H "Content-Type: application/json"  http://localhost:8080/api/user/username_01
+echo
+echo
+sleep 1
+
+# List all items
+echo "List all items"
+curl -H "Content-Type: application/json"  http://localhost:8080/api/item
+echo
+echo
+sleep 1
+
+# Get item by id
+echo "Get item by id"
+curl -H "Content-Type: application/json"  http://localhost:8080/api/item/1
+echo
+echo
+sleep 1
+
+# Get item by name
+echo "Get item by name"
+curl -H "Content-Type: application/json"  http://localhost:8080/api/item/name/Round%20Widget
+echo
+echo
+sleep 1
+
+# Add item to cart
+echo "Adding item to cart"
+curl -H "Content-Type: application/json" -d @helper/cart_add_01.json http://localhost:8080/api/cart/addToCart
+echo
+echo
+sleep 1
+
+# Add second item to cart
+echo "Adding second item to cart"
+curl -H "Content-Type: application/json" -d @helper/cart_add_02.json http://localhost:8080/api/cart/addToCart
+echo
+echo
+sleep 1
+
+# Remove item to cart
+echo "Removing item to cart"
+curl -H "Content-Type: application/json" -d @helper/cart_add_02.json http://localhost:8080/api/cart/removeFromCart
+echo
+echo
+sleep 1
+
+# Submit order
+echo "Submit order"
+curl -X POST -H "Content-Type: application/json" http://localhost:8080/api/order/submit/username_01
+echo
+echo
+sleep 1
+
+# Submit order
+echo "View order history"
+curl -H "Content-Type: application/json" http://localhost:8080/api/order/history/username_01
+echo
+echo
